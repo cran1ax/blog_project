bca1c6a07e2b06c4ecf9f1e1e3890a3b62ce96b3
cran1ax <pingatsamarth2@gmail.com>
2025-10-02 13:09:20 +0530

    user authentication

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# blog_project/settings.py
+# ... at the very end of the file
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
